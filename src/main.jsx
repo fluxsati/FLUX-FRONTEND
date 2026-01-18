06c98b31406a5dc9566b52b822f8e03a75b04ebb
Mahesh Kushwah <maheshkushwah236@gmail.com>
2026-01-18 13:09:09 +0530

    vercelspeed

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import './index.css'; // <--- ADD THIS LINE (or whatever your css file is named)
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />
+    <SpeedInsights />
   </Provider>
 );
